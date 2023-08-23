b164115e90afb1b9dcd0d8984438c67dee7f8c10
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-08-23 18:51:22 +0300

    feat: use cnaps number with facade pattern

--- src/main/java/com/api/app/repository/implementation/RepositoryImpl.java
+++ src/main/java/com/api/app/repository/implementation/RepositoryImpl.java
@@ -2,11 +2,10 @@ package com.api.app.repository.implementation;
 
 import com.api.app.cnaps.mapper.CnapsMapper;
 import com.api.app.cnaps.model.CnapsEmployee;
-import com.api.app.cnaps.repository.CnapsRepository;
 import com.api.app.model.Employee;
 import com.api.app.model.exception.NotFoundException;
-import com.api.app.repository.jpa.EmployeeRepository;
 import com.api.app.repository.Repository;
+import com.api.app.repository.facade.EmployeeFacade;
 import lombok.AllArgsConstructor;
 
 import java.util.Optional;
@@ -15,32 +14,31 @@ import java.util.Optional;
 @org.springframework.stereotype.Repository
 @AllArgsConstructor
 public class RepositoryImpl implements Repository {
-    private final EmployeeRepository employeeJpaRepository;
-    private final CnapsRepository cnapsRepository;
+    private final EmployeeFacade employeeFacade;
     private final CnapsMapper cnapsMapper;
 
     @Override
     public Employee save(Employee employee) {
-        CnapsEmployee cnapsEmployee = cnapsRepository.findByEndToEndId(employee.getId()).orElse(null);
+        CnapsEmployee cnapsEmployee = employeeFacade.findByEndToEndId(employee.getId()).orElse(null);
         if (cnapsEmployee != null){
-            return employeeJpaRepository.save(employee);
+            return employeeFacade.save(employee);
         }
-        return employeeJpaRepository.save(cnapsMapper.toDomain(employee, cnapsEmployee));
+        return employeeFacade.save(cnapsMapper.toDomain(employee, cnapsEmployee));
     }
 
     @Override
     public Employee getEmployeeById(String id) {
-        CnapsEmployee cnapsEmployee = cnapsRepository.findByEndToEndId(id).orElse(null);
-        Employee employee = employeeJpaRepository.findById(id)
+        CnapsEmployee cnapsEmployee = employeeFacade.findByEndToEndId(id).orElse(null);
+        Employee employee = employeeFacade.findById(id)
                 .orElseThrow(() -> new NotFoundException("Employee not found"));
         if (cnapsEmployee != null) {
-            return employeeJpaRepository.save(cnapsMapper.toDomain(employee, cnapsEmployee));
+            return employeeFacade.save(cnapsMapper.toDomain(employee, cnapsEmployee));
         }
-        return employeeJpaRepository.save(employee);
+        return employee;
     }
 
     @Override
     public Optional<Employee> findByPrincipalUsername(String username) {
-        return employeeJpaRepository.findByPrincipalUsername(username);
+        return employeeFacade.findByPrincipalUsername(username);
     }
 }
